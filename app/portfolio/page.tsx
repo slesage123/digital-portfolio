8a0be5e9dac773f06e7afc179fae1a1887133c18
slesage123 <slesage@mcad.edu>
2025-03-13 17:51:38 -0500

    setting up about page and portfolio cols

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,6 +4,41 @@ export default function Page(){
         <div>
             <main>
                 Portfolio
+                <div className="columns-1 gap-4 sm:columns-3 sm:gap-8 p-4">
+                    <div className="card card-dash shadow-sm p-4">
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                    </div>
+                    <div className="card card-dash shadow-sm p-4">
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                    </div>
+                    <div className="card card-dash shadow-sm p-4">
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                    </div>
+                </div>
             </main>
         </div>
     );
