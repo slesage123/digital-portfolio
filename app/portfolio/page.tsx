a25695d215b66fadc44b7f0b7dce6f853b35c6b4
slesage123 <slesage@mcad.edu>
2025-03-14 13:08:26 -0500

    adding content to portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,42 +1,106 @@
+import React from "react";
 
 export default function Page(){
     return (
         <div>
             <main>
                 Portfolio
-                <div className="columns-1 gap-4 sm:columns-3 sm:gap-8 p-4">
-                    <div className="card card-dash shadow-sm p-4">
-                        <div className="card-body">
+                <div className="auto-rows-auto columns-1 gap-4 sm:columns-3 sm:gap-8 p-4">
+                    <div className="card image-full shadow-sm p-4">
+                        <figure>
+                            <img
+                                src="/boxrender.jpg"
+                                alt="Packaging design render of a box for cosmetics."
+                            ></img>
+                        </figure>
+                        <div className="card-body p-4">
                             <h2 className="card-title">Card Title</h2>
                             <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                         </div>
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
-                        </figure>
                     </div>
-                    <div className="card card-dash shadow-sm p-4">
-                        <div className="card-body">
+                    <div className="card image-full shadow-sm p-4">
+                        <figure>
+                            <img
+                                src="/SleepTokenFull_PrintFlats_1.jpg"
+                                alt="Flat layout design for a vinyl record sleeve."
+                            ></img>
+                        </figure>
+                        <div className="card-body p-4 ">
                             <h2 className="card-title">Card Title</h2>
                             <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                         </div>
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
-                        </figure>
                     </div>
-                    <div className="card card-dash shadow-sm p-4">
-                        <div className="card-body">
+                    <div className="card image-full shadow-sm p-4">
+                        <figure>
+                            <img
+                                src="/LeSage_Spreadsheets.jpg"
+                                alt="Typeface anatomy practice poster featuring Bodoni 72."
+                            ></img>
+                        </figure>
+                        <div className="card-body p-4">
                             <h2 className="card-title">Card Title</h2>
                             <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                         </div>
+                    </div>
+                    <div className="card image-full shadow-sm p-4">
                         <figure>
                             <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
+                                src="/Lesage_s_PlaceAnalysis5_Sequencing_F.jpg"
+                                alt="Double exposure photography edited to bring out the rich colors of the night lighting."
+                            ></img>
                         </figure>
+                        <div className="card-body p-4">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
+                    </div>
+                    <div className="card image-full shadow-sm p-4">
+                        <figure>
+                            <img
+                                src="/Untitled-1.jpg"
+                                alt="Mock-up advertising for a social media ad campaign for pet insurance."
+                            ></img>
+                        </figure>
+                        <div className="card-body p-4 ">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
+                    </div>
+                    <div className="card image-full shadow-sm p-4">
+                        <figure>
+                            <img
+                                src="/netimpact-1.png"
+                                alt="Documentation of a project meant to explore the concept of glitch."
+                            ></img>
+                        </figure>
+                        <div className="card-body p-4">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
+                    </div>
+                    <div className="card image-full shadow-sm p-4">
+                        <figure>
+                            <img
+                                src="/TeaPackage.jpg"
+                                alt="Packaging design for a box of lavender tea."
+                            ></img>
+                        </figure>
+                        <div className="card-body p-4">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
+                    </div>
+                    <div className="card image-full shadow-sm p-4">
+                        <figure>
+                            <img
+                                src="/goodeats_1.jpg"
+                                alt="Magazine cover mock-up design."
+                            ></img>
+                        </figure>
+                        <div className="card-body p-4">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                        </div>
                     </div>
                 </div>
             </main>
