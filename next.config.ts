6468925313a5f3e2e23939da82b1595eab4e86ce
slesage123 <slesage@mcad.edu>
2025-03-20 12:16:03 -0500

    setting up for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/digital-portfolio",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
